89bd4579de4e1da2589bcd8c9d3913257ed93130
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-09-22 18:58:07 +0600

    update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const bodyParser= require('body-parser');
 const cors= require('cors');
 require('dotenv').config();
 const app = express();
 
 
-const port = process.env.PORT || 5022;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5cuvtq3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cpoqr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const port = process.env.PORT || 5022;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,7 +22,7 @@ app.get('/', (req, res) => {
 })
 
 client.connect(err => {
-  const collection = client.db("bengalDish").collection("breakfast");
+  const collection = client.db("bengalDish").collection("mealItems");
   // perform actions on the collection object
 
   console.log('database connected successfully')
